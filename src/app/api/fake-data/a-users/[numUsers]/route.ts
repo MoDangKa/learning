e5c9f051f18251228006922f16eefe2c4e7e3a54
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-03-31 13:37:49 +0700

    feat: create fake-date endpoint

---
+++ src/app/api/fake-data/a-users/[numUsers]/route.ts
@@ -0,0 +1,79 @@
+import { handleAxiosError } from "@/lib/apiService";
+import { faker } from "@faker-js/faker";
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const envSchema = z.object({
+  MODE: z.enum(["development", "production", "test"]),
+  API_URL: z.string().min(1),
+  JWT_TOKEN: z.string().min(1),
+});
+
+const env = envSchema.parse(process.env);
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { numUsers: string } }
+) {
+  if (env.MODE !== "development") {
+    return NextResponse.json(
+      { error: "This endpoint is for development only" },
+      { status: 401 }
+    );
+  }
+
+  const { numUsers: paramNumUsers } = await params;
+  const numUsers = parseInt(paramNumUsers);
+
+  if (typeof numUsers !== "number" || numUsers <= 0) {
+    return NextResponse.json(
+      { error: "Invalid numUsers parameter" },
+      { status: 400 }
+    );
+  }
+
+  const token = request.cookies.get(env.JWT_TOKEN)?.value;
+
+  if (!token) {
+    return NextResponse.json(
+      { error: "Authentication token missing or invalid" },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const registerList = [];
+    for (let i = 0; i < numUsers; i++) {
+      const data = {
+        email: faker.internet.email(),
+        password: "thankyou123",
+        name: faker.person.fullName(),
+        photo: faker.image.avatar(),
+      };
+
+      registerList.push(() =>
+        axios.post(`${env.API_URL}/register`, data, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+      );
+    }
+
+    const throttleLimit = 10;
+    const pLimit = (await import("p-limit")).default;
+    const limit = pLimit(throttleLimit);
+
+    const results = await Promise.all(registerList.map((fn) => limit(fn)));
+
+    const successes = results.filter((res) => res.status === 201).length;
+    return NextResponse.json(
+      { success: true, registered: successes },
+      { status: 201, statusText: "Fake data is successful" }
+    );
+  } catch (error: unknown) {
+    return handleAxiosError(error);
+  }
+}
---
+++ src/app/api/fake-data/b-users/[numUsers]/route.ts
@@ -0,0 +1,65 @@
+import { getClient } from "@/lib/database";
+import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const envSchema = z.object({
+  MODE: z.enum(["development", "production", "test"]),
+});
+
+const env = envSchema.parse(process.env);
+const SALT_ROUNDS = 10;
+
+function handleErrorResponse(error: unknown) {
+  console.error("Error occurred:", error);
+  return NextResponse.json(
+    { error: "An unexpected error occurred" },
+    { status: 500 }
+  );
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { numUsers: string } }
+) {
+  if (env.MODE !== "development") {
+    return NextResponse.json(
+      { error: "This endpoint is for development only" },
+      { status: 401 }
+    );
+  }
+
+  const { numUsers: paramNumUsers } = await params;
+  const numUsers = parseInt(paramNumUsers);
+
+  if (!Number.isInteger(numUsers) || numUsers <= 0) {
+    return NextResponse.json(
+      { error: "numUsers must be a positive integer" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const client = await getClient();
+
+    for (let i = 0; i < numUsers; i++) {
+      const email = faker.internet.email();
+      const name = faker.person.fullName();
+      const photo = faker.image.avatar();
+      const hash = await bcrypt.hash("strings123", SALT_ROUNDS);
+
+      await client.query(
+        "INSERT INTO public.users (email, password, name, photo) VALUES ($1, $2, $3, $4)",
+        [email, hash, name, photo]
+      );
+    }
+
+    return NextResponse.json(
+      { success: true },
+      { status: 201, statusText: "Fake data creation successful" }
+    );
+  } catch (error: unknown) {
+    return handleErrorResponse(error);
+  }
+}
