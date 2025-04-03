acf1f45c40ea512298a88b2e77d524ef2d9c9c59
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-04-03 09:49:34 +0700

    feat: create Avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { Fragment } from "react";
+
+type Props = { src?: string; alt: string; size?: number };
+
+function Avatar({ src, alt, size = 50 }: Props) {
+  return (
+    <div
+      className="bg-slate-600 rounded-full overflow-hidden object-cover relative"
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        maxWidth: `${size}px`,
+        maxHeight: `${size}px`,
+      }}
+    >
+      {src ? (
+        <Image
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-contain"
+          alt={alt}
+          src={src}
+          width={size}
+          height={size}
+        />
+      ) : (
+        <Fragment />
+      )}
+    </div>
+  );
+}
+
+export default Avatar;
